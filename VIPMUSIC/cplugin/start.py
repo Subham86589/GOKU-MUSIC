1e12f60c59606578380c95df89ef4d4f71f95be2
Subham86589 <167736439+Subham86589@users.noreply.github.com>
2024-05-13 16:18:38 +0530

    Update start.py

--- VIPMUSIC/cplugin/start.py
+++ VIPMUSIC/cplugin/start.py
@@ -40,9 +40,7 @@ SPAM_WINDOW_SECONDS = 5
 
 
 YUMI_PICS = [
-    "https://telegra.ph/file/3134ed3b57eb051b8c363.jpg",
-    "https://telegra.ph/file/5a2cbb9deb62ba4b122e4.jpg",
-    "https://telegra.ph/file/cb09d52a9555883eb0f61.jpg",
+    "https://graph.org/file/93cecc883ebdf7b42b938.jpg"
 ]
 
 
