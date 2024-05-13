edd77c96ee44036609c859327dfc6016b1714e4e
Subham86589 <167736439+Subham86589@users.noreply.github.com>
2024-05-13 15:57:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
+"https://telegra.ph/file/b5deb6e69fa3198f7df1d.jpg",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
